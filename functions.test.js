851a8c4a1122887f9045e504ee87772557eacf8a
oleg9952 <oleg.parhomenko2599@gmail.com>
2019-07-31 11:11:02 +0300

    commit 3

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,52 @@
 const functions = require('./functions');
 
-test('Add', () => {
-    expect(functions.add(2, 2)).toBe(4)
+// toBe
+test('Adds 2+2 to equal 4', () => {
+    expect(functions.add(2, 2)).toBe(4);
 })
+
+test('Adds 2+2 to equal 4', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+})
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+
+// toBeNull
+test('should be null', () => {
+    expect(functions.isNull()).toBeNull()
+})
+
+// toBeFalsy
+test('should be falsy', () => {
+    expect(functions.checkValue(undefined)).toBeFalsy();
+})
+
+// toBeFalsy
+test('User should be Brad Travesy object', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Brad',
+        lastName: 'Traversy'
+    })
+})
+
+// less than and greater than
+// an example of a function inside of the test
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+    // expect(load1 + load2).toBeLessThanOrEqual(1600);
+})
+
+// regular expressions
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/)
+})
+
+
