3ff4d74e1adc4bd0ab68901035277b37119cdba1
oleg9952 <oleg.parhomenko2599@gmail.com>
2019-08-26 00:07:08 +0300

    commit 7

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+//const axios = require('axios');
 
 const functions = {
     add: (num1, num2) => num1 + num2,
@@ -21,29 +21,29 @@ const functions = {
             .catch(error => console.log('Error!'))
     },
     readUsers: () => {
-        // let xhr = new XMLHttpRequest();
-        // xhr.open('GET', './Users.json');
-        // xhr.onload = function () {
-        //     if(this.status == 200) {
-        //         let data = JSON.parse(this.responseText);
-        //         data.forEach(item => {
-        //             console.log(item.name)
-        //         })
-        //     }
-        // }
-        // xhr.send();
-        
-        axios.get('./Users.json')
-            .then(resp => {
-                resp.data.forEach(item => {
-                    console.log(item.name);
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', './Users.json');
+        xhr.onload = function () {
+            if(this.status == 200) {
+                let data = JSON.parse(this.responseText);
+                data.forEach(item => {
+                    console.log(item.name)
                 })
-            })
-            .catch(error => 'Error!')
+            }
+        }
+        xhr.send();
+        
+        // axios.get('./Users.json')
+        //     .then(resp => {
+        //         resp.data.forEach(item => {
+        //             console.log(item.name);
+        //         })
+        //     })
+        //     .catch(error => 'Error!')
     }
 }
 
 functions.readUsers();
 
-module.exports = functions;
+//module.exports = functions;
 
